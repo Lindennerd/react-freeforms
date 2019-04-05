c980575a873f6eadf7e32a25526d3161ab741ba8
luiz.paulo <luiz.paulo@powerbrasil.com.br>
2019-04-05 10:19:34 -0300

    styling

--- components/layout.js
+++ components/layout.js
@@ -2,8 +2,15 @@ import Grid from '@material-ui/core/Grid';
 
 const Layout = props => (
     <div>
+        <style jsx global>
+            {` body { 
+                background-color: #eee;
+                padding: 0 2em 0 2em;
+            } `}
+        </style>
+            
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-        <Grid container>
+        <Grid container spacing={16} >
             {props.children}
         </Grid>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-import Layout from '../components/layout';
+import Layout from "../components/layout";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
 
-const Index = () => (
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+});
+
+function Index(props) {
+  const { classes } = props;
+
+  return (
     <Layout>
-        <h1>Ola Mundo</h1>
+      <Grid item xs>
+        <Paper className={classes.root} elevation={1}>
+          Item 1
+        </Paper>
+      </Grid>
+      <Grid item xs>
+        <Paper className={classes.root} elevation={1}>
+          Item 2
+        </Paper>
+      </Grid>
     </Layout>
-)
+  );
+}
 
-export default Index;
+export default withStyles(styles)(Index);
